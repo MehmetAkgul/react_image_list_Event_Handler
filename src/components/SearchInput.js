0cf7708fc562cc67fe0a930b6647d0012cc8549b
mehmetakgul <mehmetakgul.dev@gmail.com>
2022-08-21 17:19:28 +0300

    bind the onFormSubmit function

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -5,17 +5,25 @@ class SearchInput extends React.Component {
     constructor(props) {
         super(props);
         this.state = {entry: ''};
+
+        //bind the onFormSubmit function
+        this.onFormSubmit = this.onFormSubmit.bind(this)
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+        console.log(this.state.entry)
     }
 
     render() {
         return (
             <div className='ui segment'>
-                <form action="" className='ui form'>
+                <form onSubmit={this.onFormSubmit} action="" className='ui form'>
                     <div className="field">
                         <div className="ui massive icon input">
                             <input type="text"
                                    placeholder='search...'
-                                   onChange={(event) => this.setState({entry: event.target.value.toUpperCase()})}
+                                   onChange={(event) => this.setState({entry: event.target.value})}
                                    value={this.state.entry}
                             />
                             <i className="search icon"></i>
