2999f97b14351fd213e4663d81ab7b2cf979aced
mehmetakgul <mehmetakgul.dev@gmail.com>
2022-08-21 17:56:47 +0300

    bind component used with async

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,19 @@ import axios, {Axios} from "axios";
 import SearchInput from "./SearchInput";
 
 class App extends React.Component {
+    state = {images: []}
 
-    async onSearchSubmit(entry) {
+    onSearchSubmit = async (entry) => {
         const response = await axios.get(`https://pixabay.com/api/?key=29417782-ff3bb531e2194a82724270d90&q=${entry}&image_type=photo`)
-        console.log(response.data.hits)
+        //   console.log(response.data.hits)
+        this.setState({images: response.data.hits})
     }
 
     render() {
         return (
             <div className='ui container' style={{marginTop: '30px'}}>
                 <SearchInput onSearchSubmit={this.onSearchSubmit}/>
+                We have {this.state.images.length} images
             </div>
         )
     }
